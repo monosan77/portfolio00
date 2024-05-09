cf7ab617e1ef9117c5dc6d9e362d64f69cb7a4a4
monosan77 <nogu.y-soccer.1o0706@ezweb.ne.jp>
2024-05-09 15:51:48 +0900

    positionを追加してレイヤー順位を付けた

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Mainvisual } from "./Mainvisual/Mainvisual";
 import { MainContents } from "./MainContents/MainContents";
+import styles from "./Main.module.css";
 
 export function Main() {
   return (
-    <main>
+    <main className={styles.main}>
       <Mainvisual />
       <MainContents />
     </main>
